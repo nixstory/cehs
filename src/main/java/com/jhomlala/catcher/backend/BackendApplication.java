031485d7dda157ef2e873731103ae10b45270898
TaeHeun Lee <nixstory@gmail.com>
2022-01-20 14:40:36 +0900

    backend Cipher 추가

--- src/main/java/com/jhomlala/catcher/backend/BackendApplication.java
+++ src/main/java/com/jhomlala/catcher/backend/BackendApplication.java
@@ -3,10 +3,15 @@ package com.jhomlala.catcher.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.reactspring.util.Cipher;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		String encText = Cipher.encrypt("123456", "SHA-256");
+		System.out.println(encText.length());
+		
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
